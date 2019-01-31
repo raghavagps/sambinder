82becac347e075de701fcbe0b66b367ec4d45307
Gaurav Mishra <in.gaurav.mishra@gmail.com>
2019-01-31 17:59:37 +0530

    Update predict.py

--- code/predict.py
+++ code/predict.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 #_*_coding:utf-8_*_
-
-
 from sklearn.svm import SVC
 from sklearn.datasets import load_svmlight_file
 from sklearn.externals import joblib
@@ -12,48 +10,64 @@ import pandas as pd
 import sys
 import csv
 import itertools
+
 def predict(testdata, method,threshold):
     
+    #print('Test data',testdata,'\n\n')
     df = pd.DataFrame();
     y_p_score1=pd.DataFrame();
     data_test = pd.DataFrame(testdata);
     X_test = data_test.iloc[:][0:356];
+   
     a=[];
     k=0;
     
     if(method==1):
-        clf = joblib.load('/Users/gaurav/Desktop/sambinder/Trained Models/svc_binary_model')
+        path=currdir+'/sam_models/svc_binary_model';
+        clf = joblib.load(path)
         y_p_score1=clf.decision_function(X_test)
+        #print(y_p_score1);
         
     elif(method==2):
-        clf = joblib.load('/Users/gaurav/Desktop/sambinder/Trained Models/rf_binary_model')
+        path=currdir+'/sam_models/rf_binary_model';
+        clf = joblib.load(path)
         y_p_score=clf.predict_proba(X_test)
         y_p_score1 = [i[1] for i in y_p_score] 
         
     elif(method==3):
-        clf = joblib.load('/Users/gaurav/Desktop/sambinder/Trained Models/ann_binary_model')
+        path=currdir+'/sam_models/ann_binary_model';
+        clf = joblib.load(path)
         y_p_score=clf.predict_proba(X_test)
-        y_p_score1 = [i[1] for i in y_p_score] 
-        
+        y_p_score1 = [i[1] for i in y_p_score]
+ 
     elif(method==4):
-        clf = joblib.load('/Users/gaurav/Desktop/sambinder/Trained Models/svc_pssm_model')
-        y_p_score1=clf.decision_function(X_test)
+        path=currdir+'/sam_models/knn_binary_model';
+        clf = joblib.load(path)
+        y_p_score=clf.predict_proba(X_test)
+        y_p_score1 = [i[1] for i in y_p_score]
     
     elif(method==5):
-        clf = joblib.load('/Users/gaurav/Desktop/sambinder/Trained Models/rf_pssm_model')
+        path=currdir+'/sam_models/svc_pssm_model';
+        clf = joblib.load(path)
+        y_p_score1=clf.decision_function(X_test)
+    
+    elif(method==6):
+        path=currdir+'/sam_models/rf_pssm_model';
+        clf = joblib.load(path)
         y_p_score=clf.predict_proba(X_test)
         y_p_score1 = [i[1] for i in y_p_score] 
-        
-    interact =[];
-   
-    y_p_score2=normalise(pd.DataFrame(y_p_score1));
+       
+    interact ='';
+    new_score='';
     
+    y_p_score2=normalise(pd.DataFrame(y_p_score1));    
     y_p_score = pd.DataFrame(y_p_score2);
     y_p_score=y_p_score.astype(int);
     for i in range(len(y_p_score)):
         if(int(y_p_score.iloc[i][0])<threshold):
-            interact.extend('-');
+            interact=interact+'-';
         else:
-            interact.extend('+');  
-            
-    return y_p_score,interact;
+            interact=interact+'+'; 
+        new_score=new_score+str(y_p_score.iloc[i][0])
+       
+    return new_score,interact;
